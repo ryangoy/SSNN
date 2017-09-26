a57af2954ec653458dcb96da46b3d7193dcabc7e
Ryan Goy <rgoy@rgoy-macbookpro.roam.corp.google.com>
2017-09-25 17:20:10 -0700

    python boilerplate code for probe layers done

--- src/cpp/field_probing_layer.hpp
+++ src/cpp/field_probing_layer.hpp
@@ -1,62 +1,55 @@
-#ifndef FIELD_PROBING_LAYER_HPP_
-#define FIELD_PROBING_LAYER_HPP_
-
-#include <boost/random.hpp>
+#ifndef TENSORFLOW_FIELD_PROBING_LAYER_HPP_
+#define TENSORFLOW_FIELD_PROBING_LAYER_HPP_
 
 
-// tf code?
+#include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
+#include "tensorflow/core/framework/resource_mgr.h"
+#include "tensorflow/core/platform/mem.h"
+#include "tensorflow/core/util/tensor_format.h"
+
+#if GOOGLE_CUDA
+#include "tensorflow/core/kernels/conv_ops_gpu.h"
+#include "tensorflow/core/platform/stream_executor.h"
+#endif  // GOOGLE_CUDA
+
+namespace tensorflow {
+
+// Forward declaration.
+class OpKernelContext;
+
 template <typename Device, typename T>
-struct ExampleFunctor {
-  void operator()(const Device& d, int size, const T* in, T* out);
+struct LaunchProbe3DOp {
+  void operator()(OpKernelContext* ctx, bool use_cudnn, bool cudnn_use_autotune,
+                  const Tensor& input, const Tensor& filter, int row_stride,
+                  int col_stride, const Padding& padding, Tensor* output,
+                  TensorFormat data_format);
+}
+
+#ifdef GOOGLE_CUDA
+template <typename T>
+struct LaunchProbe3DOp<Eigen::GpuDevice, T> {
+  void operator()(OpKernelContext* ctx, bool use_cudnn, bool cudnn_use_autotune,
+                  const Tensor& input, const Tensor& filter, int row_stride,
+                  int col_stride, const Padding& padding, Tensor* output,
+                  TensorFormat data_format);
+};
+#endif  // GOOGLE_CUDA
+
+// Used to keep track of persistent memory buffers used within the op.
+// It uses malloc and free to avoid the time cost of initializing the memory.
+template <class T, size_t size>
+struct Im2ColBufferResource : public ResourceBase {
+  Im2ColBufferResource<T, size>() {
+    data = static_cast<T*>(port::Malloc(size * sizeof(T)));
+  }
+  ~Im2ColBufferResource<T, size>() { port::Free(data); }
+  // This mutex ensures that only a single operation at a time is able to use
+  // the buffer memory held by this resource.
+  mutex mu;
+  T* data;
+  string DebugString() { return "Im2ColBufferResource"; }
 };
 
-/**
- * @brief FieldProbingLayer
- */
-template <typename Dtype>
-class FieldProbingLayer : public Layer<Dtype> {
-public:
-  explicit FieldProbingLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual inline const char* type() const { return "FieldProbing"; }
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+}  // namespace tensorflow
 
-  virtual inline int MinBottomBlobs() const { return 1; }
-  virtual inline int MinTopBlobs() const { return 1; }
-  virtual inline bool EqualNumBottomTopBlobs() const { return true; }
-
-protected:
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-
-  void InitializeFilters(Blob<Dtype>* blob, const FieldProbingParameter& param);
-  typedef boost::variate_generator<caffe::rng_t*, boost::uniform_real<Dtype> > VariateGenerator;
-  static void ForceInRange(const Dtype x, const Dtype y, const Dtype z, Dtype& xx, Dtype& yy, Dtype& zz);
-  static void SampleOnSphere(Dtype radius, Dtype& x, Dtype& y, Dtype& z, VariateGenerator& variate_generator);
-  static void SampleOnHalfSphere(Dtype radius, Dtype& x, Dtype& y, Dtype& z, VariateGenerator& variate_generator);
-
-  int field_num_;
-  int batch_size_;
-  int field_dim_;
-
-  int num_sliding_;
-  Dtype padding_;
-
-  int num_filter_;
-  int len_filter_;
-
-  static const int len_coordinates = 4;
-};
-
-
-
-#endif  // FIELD_PROBING_LAYER_HPP_
+#endif  // TENSORFLOW_KERNELS_CONV_OPS_H
