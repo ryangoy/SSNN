84c14f25fa4e48f3bd5b76803d1f5ccc5a4e948c
Ryan Goy <ryangoy@gmail.com>
2017-10-13 02:37:55 -0700

    deleted old code and formatted main and utils py fiiles

--- src/python/3d_localization.py
+++ src/python/3d_localization.py
@@ -9,99 +9,39 @@ import tensorflow as tf
 import numpy as np
 from os.path import join, isdir
 from os import listdir
+from utils import get_dims, normalize_pointclouds, load_points
 
+# Tensorflow flags boilerplate code.
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 
+# Define user inputs.
 flags.DEFINE_string('data_dir', '/home/ryan/cs/datasets/SSNN/test', 
                     'Path to base directory.')
 flags.DEFINE_integer('num_epochs', 1, 'Number of epochs to train.')
 flags.DEFINE_float('val_split', 0.1, 'Percentage of input data to use as test.')
 
+# Define constant paths
 X_NPY         = join(FLAGS.data_dir, 'input_data.npy')
 YS_NPY        = join(FLAGS.data_dir, 'segmentation_data.npy')
 YL_NPY        = join(FLAGS.data_dir, 'label_data.npy')
 OUTPUT_PATH   = join(FLAGS.data_dir, 'predictions.npy')
 
-def load_from_directory(path):
-  """
-  Assumes dataset structure is as follows:
-  base
-    area_1
-      room_1
-      Annotations
-      room_1.txt
-      ...
-      room_n
-    ...
-    area_n
-  """
-  input_data = []
-  segmentations = []
-  labels = []
-  # Loop through Areas
-  for area in listdir(path):
-    print "Loading area {}...".format(area)
-    area_path = join(path, area)
-    if not isdir(area_path):
-      continue
-      
-    # Loop through rooms
-    for room in listdir(area_path):
-      print "\tLoading room {}...".format(room)
-      room_path = join(area_path, room)
-      if not isdir(room_path) or room.endswith('Angle.txt') or room == '.DS_Store':
-        continue
-        
-      # Load point cloud
-      input_pc = np.loadtxt(join(room_path, room+'.txt'), dtype=np.float32)
-      
-      # Loop and load Annotations folder
-      annotation_pc = []
-      annotation_label = []
-      for annotation in listdir(join(room_path, 'Annotations')):
-        if annotation.startswith('wall') or annotation.startswith('ceiling') 
-                                         or not annotation.endswith('.txt'):
-          continue
-        annotation_pc.append(np.loadtxt(join(room_path, 'Annotations', annotation), 
-                                        dtype=np.float32))
-        annotation_label.append(annotation.split('.')[0])
-      annotation_pc = np.array(annotation_pc)
-      
-      input_data.append(input_pc)
-      segmentations.append(annotation_pc)
-      labels.append(annotation_label)
-
-  input_data = np.array(input_data)
-  segmentations = np.array(segmentations)
-  labels = np.array(labels)
-
-  # print input_data.shape
-  # print segmentations.shape
-  # print labels.shape
-
-  return input_data, segmentations, labels
-
-def load_from_npy(X_path, ys_path, yl_path):
-  return np.load(X_path), np.load(ys_path), np.load(yl_path)
-
 def main(_):
 
-  # Load data from preloaded npy files or from directory.
-  if FLAGS.load_from_npy:
-    X, ys, yl = load_from_npy(X_NPY, YS_NPY, YL_NPY)
-  else:
-    X, ys, yl = load_from_directory(FLAGS.data_dir)
-    np.save(X_NPY_PATH, X)
-    np.save(YS_NPY_PATH, ys)
-    np.save(YL_NPY_PATH, yl)
+  X_raw, ys, yl = load_points(path=None, npy_path=None, load_from_npy=True)
 
-  # Preprocess input.
-  ### TO DO ###
-  # -remove outliers
-  # -align to nearest 90 degree angle
-  # -remove walls?
-  # -data augmentation
+  # Get the dimensions of the first room. 
+  room_dims = get_dims(X_raw[0])
+
+  # Shift to the same coordinate space between pointclouds.
+  X = normalize_pointclouds(X_raw)
+
+  # TODO: Preprocess input.
+  # - remove outliers
+  # - align to nearest 90 degree angle
+  # - remove walls?
+  # - data augmentation
 
   # Initialize model. max_room_dims and step_size are in meters.
   ssnn = SSNN(max_room_dims, step_size)
@@ -121,5 +61,6 @@ def main(_):
   # Save output.
   np.save(OUTPUT_PATH, preds)
 
+# Tensorflow boilerplate code.
 if __name__ == '__main__':
   tf.app.run()
