db1d2f50142d9474e8c6bced4e1853ae0cb27997
ryan <ryangoy@gmail.com>
2017-11-29 18:54:28 -0800

    fixed bug with dims

--- src/python/main.py
+++ src/python/main.py
@@ -27,7 +27,7 @@ flags.DEFINE_string('data_dir', '/home/ryan/cs/datasets/SSNN/buildings',
                     'Path to base directory.')
 flags.DEFINE_bool('load_from_npy', True, 'Whether to load from preloaded \
                     dataset')
-flags.DEFINE_integer('num_epochs', 30, 'Number of epochs to train.')
+flags.DEFINE_integer('num_epochs', 50, 'Number of epochs to train.')
 flags.DEFINE_float('val_split', 0.1, 'Percentage of input data to use as test.')
 flags.DEFINE_integer('num_steps', 16, 'Number of intervals to sample\
                       from in each xyz direction.')
@@ -43,7 +43,7 @@ flags.DEFINE_bool('load_probe_output', True, 'Load the probe output if a valid f
 NUM_SCALES = 3
 
 # Define sets for training and testing
-TRAIN_AREAS = ['Area_1', 'Area_2', 'Area_3', 'Area_4', 'Area_5']
+TRAIN_AREAS = ['Area_1']
 TEST_AREAS = ['Area_6']
 
 # Define constant paths
@@ -54,36 +54,35 @@ output_dir = join(FLAGS.data_dir, 'outputs')
 if not exists(output_dir):
   makedirs(output_dir)
 
-X_TRN         = join(intermediate_dir, 'trn_data.npy')
-YS_TRN        = join(intermediate_dir, 'trn_seg_labels.npy')
-YL_TRN       = join(intermediate_dir, 'trn_cls_labels.npy')
-BBOX_TRN     = join(intermediate_dir, 'trn_bboxes.npy')
-PROBE_TRN     = join(intermediate_dir, 'trn_probe_out.npy')
+X_TRN            = join(intermediate_dir, 'trn_data.npy')
+YS_TRN           = join(intermediate_dir, 'trn_seg_labels.npy')
+YL_TRN           = join(intermediate_dir, 'trn_cls_labels.npy')
+BBOX_TRN         = join(intermediate_dir, 'trn_bboxes.npy')
+PROBE_TRN        = join(intermediate_dir, 'trn_probe_out.npy')
 
-X_TEST      = join(intermediate_dir, 'test_data.npy')
-YS_TEST         = join(intermediate_dir, 'tes_seg_labels.npy')
-YL_TEST        = join(intermediate_dir, 'trn_cls_labels.npy')
-BBOX_TEST      = join(intermediate_dir, 'trn_bboxes.npy')
-PROBE_TEST     = join(intermediate_dir, 'trn_probe_out.npy')
+X_TEST           = join(intermediate_dir, 'test_data.npy')
+YS_TEST          = join(intermediate_dir, 'test_seg_labels.npy')
+YL_TEST          = join(intermediate_dir, 'test_cls_labels.npy')
+BBOX_TEST        = join(intermediate_dir, 'test_bboxes.npy')
+PROBE_TEST       = join(intermediate_dir, 'test_probe_out.npy')
 
-CLS_TRN_LABELS    = join(output_dir, 'cls_trn_labels.npy')
-LOC_TRN_LABELS    = join(output_dir, 'loc_trn_labels.npy')
-BBOX_TRN_LABELS   = join(output_dir, 'bbox_trn_labels.npy')
-CLS_TEST_LABELS    = join(output_dir, 'cls_test_labels.npy')
-LOC_TEST_LABELS    = join(output_dir, 'loc_test_labels.npy')
-BBOX_TEST_LABELS   = join(output_dir, 'bbox_test_labels.npy')
-CLS_PREDS     = join(output_dir, 'cls_predictions.npy')
-LOC_PREDS     = join(output_dir, 'loc_predictions.npy')
-NMS_PREDS     = join(output_dir, 'nms_loc_predictions.npy')
-BBOX_PREDS    = join(output_dir, 'bbox_predictions.npy')
-BBOX_CLS_PREDS= join(output_dir, 'bbox_cls_predictions.npy')
+CLS_TRN_LABELS   = join(output_dir, 'cls_trn_labels.npy')
+LOC_TRN_LABELS   = join(output_dir, 'loc_trn_labels.npy')
+BBOX_TRN_LABELS  = join(output_dir, 'bbox_trn_labels.npy')
+
+CLS_TEST_LABELS  = join(output_dir, 'cls_test_labels.npy')
+LOC_TEST_LABELS  = join(output_dir, 'loc_test_labels.npy')
+BBOX_TEST_LABELS = join(output_dir, 'bbox_test_labels.npy')
+
+CLS_PREDS        = join(output_dir, 'cls_predictions.npy')
+LOC_PREDS        = join(output_dir, 'loc_predictions.npy')
+NMS_PREDS        = join(output_dir, 'nms_loc_predictions.npy')
+BBOX_PREDS       = join(output_dir, 'bbox_predictions.npy')
+BBOX_CLS_PREDS   = join(output_dir, 'bbox_cls_predictions.npy')
 
 
 def preprocess_input(model, data_dir, areas, x_path, ys_path, yl_path, probe_path, 
-                      cls_labels, loc_labels, bbox_labels, 
- load_from_npy,
-                      load_probe_output
-                      ):
+                      cls_labels, loc_labels, bbox_labels, load_from_npy, load_probe_output):
   # TODO: Preprocess input.
   # - remove outliers
   # - align to nearest 90 degree angle
@@ -100,10 +99,9 @@ def preprocess_input(model, data_dir, areas, x_path, ys_path, yl_path, probe_pat
   # width, height, and depth dims of all rooms.
   print("Normalizing pointclouds...")
   X_cont, dims, ys = normalize_pointclouds(X_raw, ys_raw)
-
   print("Augmenting dataset...")
   X_cont, ys = augment_pointclouds(X_cont, ys)
-  
+  dims = np.array([7.5, 7.5, 7.5])
   kernel_size = dims / FLAGS.num_steps
   print("Generating bboxes...")
   bboxes = generate_bounding_boxes(ys, bbox_labels)
@@ -111,6 +109,7 @@ def preprocess_input(model, data_dir, areas, x_path, ys_path, yl_path, probe_pat
   y_cls, y_loc = create_jaccard_labels(bboxes, FLAGS.num_steps, kernel_size)
   np.save(cls_labels, y_cls)
   np.save(loc_labels, y_loc)
+  exit()
 
   # Hack-y way of combining samples into one array since each sample has a
   # different number of points.
@@ -151,6 +150,7 @@ def main(_):
   X_trn, y_trn_cls, y_trn_loc = preprocess_input(ssnn, FLAGS.data_dir, TRAIN_AREAS, X_TRN, YS_TRN, YL_TRN, PROBE_TRN, 
                       CLS_TRN_LABELS, LOC_TRN_LABELS, BBOX_TRN_LABELS, FLAGS.load_from_npy,
                       load_probe)
+
   X_test, _, _ = preprocess_input(ssnn, FLAGS.data_dir, TEST_AREAS, X_TEST, YS_TEST, YL_TEST, PROBE_TEST, 
                       CLS_TEST_LABELS, LOC_TEST_LABELS, BBOX_TEST_LABELS, FLAGS.load_from_npy,
                       load_probe)
@@ -164,11 +164,12 @@ def main(_):
   # X_val = X[:train_split]
   # y_val_cls = y_cls[:train_split]
   # y_val_loc = y_loc[:train_split]
+
   ssnn.train_val(X_trn, y_trn_cls, y_trn_loc, epochs=FLAGS.num_epochs) #y_l not used yet for localization
 
   # Test model. Using validation since we won't be using real 
   # "test" data yet. Preds will be an array of bounding boxes. 
-  cls_preds, loc_preds = ssnn.test(X_test)
+  cls_preds, loc_preds = ssnn.test(X_trn[:20])
   
   # Save output.
   cls_f, loc_f = save_output(CLS_PREDS, LOC_PREDS, cls_preds, loc_preds, 
