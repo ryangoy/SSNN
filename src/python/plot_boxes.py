e6458ae3396e3b76d0dc5d46a0154d7f1bf2942d
Ryan Goy <rgoy@berkeley.edu>
2018-04-24 14:11:53 -0700

    plot boxes bug fixes

--- src/python/plot_boxes.py
+++ src/python/plot_boxes.py
@@ -6,8 +6,11 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection, Line3DCollection
 import matplotlib.pyplot as plt
 
 
-def plot_bounding_box(bbox, ax, color):
 
+def plot_bounding_box(bbox, ax, color):
+    volume = (bbox[3]-bbox[0])*(bbox[4]-bbox[1])*(bbox[5]-bbox[2])
+    volume = abs(volume)
+    
     points = np.array([[bbox[0], bbox[1], bbox[2]],
                        [bbox[0], bbox[4], bbox[2]],
                        [bbox[0], bbox[4], bbox[5]],
@@ -19,7 +22,6 @@ def plot_bounding_box(bbox, ax, color):
 
     Z = np.zeros((8,3))
     for i in range(8): Z[i,:] = points[i,:]
-    Z = 10.0*Z
 
     ax.scatter3D(Z[:, 0], Z[:, 1], Z[:, 2])
 
@@ -33,18 +35,19 @@ def plot_bounding_box(bbox, ax, color):
      [Z[2],Z[3],Z[7],Z[6]]]
 
     # plot sides
-    collection = Poly3DCollection(verts, facecolors=None, linewidths=1, edgecolors=color, alpha=0.0)
+    collection = Poly3DCollection(verts, facecolors=None, linewidths=1, edgecolors=color, alpha=0.1)
     face_color = color # alternative: matplotlib.colors.rgb2hex([0.5, 0.5, 1])
     collection.set_facecolor(face_color)
     ax.add_collection3d(collection)
-
+    return volume
 
 
 def main():
     preds = np.load('category_preds_nms.npy')
     labels = np.load('category_labels.npy')
 
-
+    pred_vols = []
+    label_vols = []
     for scene_preds, scene_labels in zip(preds, labels):
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
@@ -52,16 +55,33 @@ def main():
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
         ax.set_zlabel('Z')
-
+        ax.set_xlim(0,8)
+        ax.set_ylim(0, 8)
+        ax.set_zlim(0, 8)
 
         #for pred in scene_preds:
-        plot_bounding_box(scene_preds[0], ax, color='b')
-
+        
+        for i in range(len(scene_labels)):
+#            if len(scene_labels) > 1:
+#                break
+            v = plot_bounding_box(scene_preds[i], ax, color='b')
+            pred_vols.append(v)
+#            break
         for label in scene_labels:
-
-            plot_bounding_box(label, ax, color='r')
-
+#            if len(scene_labels) > 1:
+#                break
+            v = plot_bounding_box(label, ax, color='r')
+            label_vols.append(v)
+            
         plt.show()
+
+    plt.hist(pred_vols, bins=25, color='b', alpha=0.5, range=(0, 100))
+    plt.hist(label_vols, bins=25, color='r', alpha=0.5, range=(0,100))
+    plt.show()
+    
+    plt.hist(np.array(pred_vols)/np.array(label_vols), range=(0, 5), bins=20, color='b')
+    plt.show()
+
 
 if __name__ == '__main__':
     main()
