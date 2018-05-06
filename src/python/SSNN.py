383d7aa32a1d0495ecda85cf934dd24b02265c76
ryan <ryangoy@gmail.com>
2018-05-06 01:53:05 -0700

    merge

--- src/python/SSNN.py
+++ src/python/SSNN.py
@@ -365,7 +365,7 @@ class SSNN:
           counter = 0
 
       # Compute validation loss and validation mAP
-      if X_val is not None and y_val_cls is not None and y_val_loc is not None:
+      if X_val is not None and y_val_cls is not None and y_val_loc is not None and epoch>3:
         val_loss = 0
         val_cls_loss = 0
         val_loc_loss = 0
